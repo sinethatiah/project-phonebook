1b33b8fe7f9ce58d4e368f8f14c78fec4a851ada
thatiah <thatiahnia@gmail.com>
2026-04-23 10:59:03 +0300

    changed footer in landing page

--- emergencies.js
+++ emergencies.js
@@ -1,26 +1,4 @@
-//  $(function () {
-//         function loadCountries(name) {
-//           $.ajax({
-//             url: `https://restcountries.com/v3.1/all?fields=${name}`,
-//             method: "GET",
-//             success: function (data) {
-//               data.sort((a, b) => a.name.common.localeCompare(b.name.common));
 
-//               data.forEach(function (country) {
-//                 $("#country-selector").append(
-//                   `<a class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-100 cursor-pointer country-option" data-code="${country}">${country.name.common}</a>`,
-//                 );
-//               });
-//             },
-//           });
-//         }
-
-//         loadCountries(name);
-//       });
-
-//       $("#dropdownBtn").on("click", function () {
-//         $("#country-selector").toggleClass("hidden");
-//       });
 $(function () {
 
   function loadCountries(name) {
